639300fca905df32ef4b79ee950f9bf7a25f2b65
rubo77 <github@r.z11.de>
2016-08-25 22:46:04 +0200

    Knoten pro Landkreis berechnet aus Gesamtzahl Nodes minus jetz immer 329 Fix-Knoten in Dittmarschen

--- api.py
+++ api.py
@@ -9,17 +9,17 @@ import json
 import shutil
 
 Landkreise={
-        "cuxhaven":[53.844976, 8.707192,"","",""],
-	"dithmarschen":[54.211523, 9.120454,"Offener Kanal Westkueste","Landvogt-Johannsen-Strasse 11, Heide","25746"],
+	"cuxhaven":[53.844976, 8.707192,"","",""],
+	#"dithmarschen":[54.211523, 9.120454,"Offener Kanal Westkueste","Landvogt-Johannsen-Strasse 11, Heide","25746"],
 	"malente":[],
-        "neumuenster":[54.085557, 9.977224,"","",""],
+	"neumuenster":[54.085557, 9.977224,"","",""],
 	"nordfriesland":[54.483218, 9.051240,"","",""],
 	"nordheide":[53.325656, 9.868644,"","",""],
 	"ploen":[54.202036, 10.431209,"","",""],
-        "rendsburg-eckernfoerde":[54.303842, 9.649755,"","",""],
-        "schleswig":[54.521868, 9.561861,"","",""],
-        "segeberg":[53.923483, 10.114170,"","",""],
-        "stade":[53.582963, 9.459456,"","",""],
+	"rendsburg-eckernfoerde":[54.303842, 9.649755,"","",""],
+	"schleswig":[54.521868, 9.561861,"","",""],
+	"segeberg":[53.923483, 10.114170,"","",""],
+	"stade":[53.582963, 9.459456,"","",""],
 	"steinburg":[53.922188, 9.518219,"","",""],
 	"sylt":[],
 	"tarp":[]
@@ -49,8 +49,8 @@ for node in nodes:
         if status['online']:
                 num_nodes += 1
 
-#Knoten pro Landkreis
-Knoten_pro_Landkreis = num_nodes/len(Landkreise)
+#Knoten pro Landkreis (Berechnet aus Gesamtzahl Nodes minus 329 Fix-Knoten in Dittmarschen)
+Knoten_pro_Landkreis = (num_nodes-329) / len(Landkreise)
 
 #Zeit holen
 thetime = datetime.datetime.now().isoformat()
@@ -74,8 +74,8 @@ for Landkreis in Landkreise:
 	#ffnord['location']['lat'] = Landkreise[Landkreis][0]
 	#ffnord['location']['lon'] = Landkreise[Landkreis][1]
 	#ffnord['location']['address']['Name'] = Landkreise[Landkreis][2]
-        #ffnord['location']['address'] ['Street']= Landkreise[Landkreis][3]
-        #ffnord['location']['address'] ['Zipcode']= Landkreise[Landkreis][4]
+	#ffnord['location']['address'] ['Street']= Landkreise[Landkreis][3]
+	#ffnord['location']['address'] ['Zipcode']= Landkreise[Landkreis][4]
 
 	#Freifunk API-Datein mit geaenderten werten schreiben
 	with open(LandkreisAPI, 'w') as fp:
